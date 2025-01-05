ecfbca4d7f101607f08cb128e6af12ddb18237a5
junnaaa <paxtonbennison@outlook.com>
2025-01-05 12:29:01 +0800

    various config changes, addition of fish config file and .gitignore file

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+hyfetch
+# Import colorscheme from 'wal' asynchronously
+# &   # Run the process in the background.
+# ( ) # Hide shell job control messages.
+# Not supported in the "fish" shell.
+
+# Alternative (blocks terminal for 0-3ms)
+cat /home/junnaaa/.cache/wal/sequences
+
+
