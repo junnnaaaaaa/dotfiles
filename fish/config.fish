84421dc85545bc8af2e97fd074c13232175cdbfb
junnnaaaaaa <paxtonbennison@outlook.com>
2025-01-17 17:41:47 +0800

    establishment of new branch

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+hyfetch
+# Import colorscheme from 'wal' asynchronously
+# &   # Run the process in the background.
+# ( ) # Hide shell job control messages.
+# Not supported in the "fish" shell.
+
+# Alternative (blocks terminal for 0-3ms)
+cat /home/junnaaa/.cache/wal/sequences
+
+
+
+# Created by `pipx` on 2025-01-07 07:14:40
+set PATH $PATH /home/junnaaa/.local/bin
